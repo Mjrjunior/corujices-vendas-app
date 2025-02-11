1131092fe1d0bee3fc4e75364e07e14ab854cee8
Mjrjunior <mjrjunior4@gmail.com>
2025-02-10 21:54:44 -0300

    implement sign-up form with API integration and validation

--- src/pages/auth/signUp.tsx
+++ src/pages/auth/signUp.tsx
@@ -8,34 +8,76 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { api } from "@/services/api";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const data = { name, email, password };
+
+      const response = await api.post("/companies", data);
+
+      if (response.status === 201) {
+        alert("Conta criada com sucesso");
+        navigate("/signin");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao criar conta");
+    }
+  };
+
   return (
     <Card className="w-[350px]">
       <CardHeader>
         <CardTitle>Login</CardTitle>
       </CardHeader>
-      <CardContent>
-        <form>
+      <form onSubmit={handleSubmit}>
+        <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">Nome</Label>
-              <Input id="name" placeholder="Nome" />
+              <Input
+                id="name"
+                placeholder="Nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="Email" />
+              <Input
+                id="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="password">Senha</Label>
-              <Input id="password" placeholder="Senha" />
+              <Input
+                id="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button>Cadastrar</Button>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button type="submit">Cadastrar</Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
